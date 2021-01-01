ebcb1db9a77ef8a6214eddd71acd4adb960cda54
Alex Studer <alex@alexstuder.ch>
2021-01-01 22:19:40 +0100

    LinkedList added

--- 2_LinkedListDemo/src/LinkedListDemo.java
+++ 2_LinkedListDemo/src/LinkedListDemo.java
@@ -1,7 +1,45 @@
+import java.util.LinkedList;
+
 public class LinkedListDemo {
 
-    public static void main(String[] args){
+    static LinkedList<String> employees ;
 
+    public static void main(String[] args) {
+
+        employees = new LinkedList<String>();
+
+        employees.add("Robert Johnson");
+        employees.add("Hendrix");
+        employees.add("Srv");
+        employees.add("B.B. King");
+
+        // ToString  auch bei ArrayList
+        System.out.println(employees.toString());
+
+        employees.addFirst("Ich bin erster");
+        printLinkedList();
+
+        employees.addLast("Ich bin letzter");
+        printLinkedList();
+
+        employees.remove(2);
+        printLinkedList();
+
+        employees.removeFirst();
+        printLinkedList();
+
+        employees.remove("Srv");
+        printLinkedList();
+
+
+    }
+
+    static void printLinkedList() {
+
+        System.out.println();
+        for (String emloyee : employees) {
+            System.out.println(emloyee);
+        }
 
 
     }
